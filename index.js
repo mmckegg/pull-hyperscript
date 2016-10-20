cdb88b8cf304a21b1530fb0539d55f8700bc8d3c
mix irving <whimful@gmail.com>
2016-10-20 23:19:47 +1300

    extract createAttributes lib

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var pullCat = require('pull-cat')
 var pull = require('pull-stream')
 
+var createAttributes = require('./lib/createAttributes')
+
 module.exports = h
 
 function h (tagName, props, children) {
@@ -18,35 +20,3 @@ function h (tagName, props, children) {
   return pullCat(things)
 }
 
-function createAttributes (attributes) {
-  var buf = ''
-
-  Object.keys(attributes).forEach(function (attribute) {
-    var value = attributes[attribute]
-    if (!truthyEnough(value)) return
-
-    value = Array.isArray(value) ? validValues(value)
-      : Object(value) === value ? validKeys(value)
-      : value
-    if (!truthyEnough(value)) return
-
-    buf += ' ' + attribute
-    if (value !== true) buf += '="' + value + '"'
-  })
-
-  return buf
-}
-
-function truthyEnough (value) {
-  return value || value === 0 || value === ''
-}
-
-function validValues (array) {
-  return array.filter(Boolean).join(' ')
-}
-
-function validKeys (object) {
-  return Object.keys(object).filter(function (key) {
-    return object[key]
-  }).join(' ')
-}
