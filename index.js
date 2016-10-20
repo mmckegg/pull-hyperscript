8c51502422dad6a6bb4745b9e45f25f3073680ff
mix irving <whimful@gmail.com>
2016-10-21 00:35:36 +1300

    fixup and actual tests :dog2:

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 var pullCat = require('pull-cat')
 var pull = require('pull-stream')
+var isArray = Array.isArray
 
 var createAttributes = require('./lib/createAttributes')
 
 module.exports = h
 
 function h (tagName, props, children) {
-  if (children == null && typeof props === 'function') {
+  if (isSkippingProps()) {
     children = props
-    props = null
+    props = {}
   }
 
   var things = []
-  things.push(pull.once(`<${tagName}${createAttributes(props || {})}>`))
-  if (children) {
-    things.push(children)
-  }
+  things.push(pull.once(`<${tagName}${createAttributes(props)}>`))
+  if (children)
+    things.push(childrenToStream(children))
   things.push(pull.once(`</${tagName}>`))
   return pullCat(things)
+
+  function isSkippingProps() {
+    return children == undefined &&
+      (typeof props === 'function' || isArray(props) || typeof props === 'string')
+  }
 }
+
+function childrenToStream (children) {
+  if (isStream(children)) return children
+
+  if (isArray(children))
+    return pullCat(children.map(childrenToStream))
+ 
+  return pull.values([children])
+}
+
+function isStream (fn) {
+  return typeof fn  === 'function'
+}
+
 
