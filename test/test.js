d2ca6086470a57991993faea7af7328b862fd635
Matt McKegg <matt@wetsand.co.nz>
2016-10-21 00:33:59 +1300

    include children in test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var h = require('../')
 var pull = require('pull-stream')
 pull(
-  h('div', {class: 'test'}),
+  h('div', {class: 'test'}, pull.values('test')),
   pull.concat((err, x) => {
     console.log(x)
   })
